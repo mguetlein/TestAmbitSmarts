bc5c6b390f7d1e9e20156978751166ed42754eff
Martin Gütlein <guetlein@posteo.de>
2016-11-07 11:37:52 +0100

    withdraw test for 2690

--- src/test/java/org/kramerlab/test/TestAmbitWithdrawn.java
+++ src/test/java/org/kramerlab/test/TestAmbitWithdrawn.java
@@ -3,7 +3,6 @@ package org.kramerlab.test;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.openscience.cdk.interfaces.IAtomContainer;
@@ -14,7 +13,30 @@ import org.openscience.cdk.smiles.SmilesParser;
 @RunWith(JUnit4.class)
 public class TestAmbitWithdrawn
 {
-	@Test
+	// https://sourceforge.net/p/ambit/bugs/108/
+	public void aromBondNotAddedInSmirksWithSmartsBonds_rule2690_c0138()
+	{
+		/**
+		 * ambit cannot introduce aromatic form out of not-arom, instead =- bonds have to be introduced 
+		 * https://github.com/enviPath/enviPath/issues/101
+		 */
+		String smirks = "[#6:9]=,:[#6:10]@-[C:6]([H])([#8:2][H:11])[C:5]([H])(@-[#6:7]=,:[#6:8])[#8:1][H:12]>>[H:12][#8:1]-[c:5](:[c:7]:[c:8]):[c:6](-[#8:2][H:11]):[c:10]:[c:9]";
+		String smi = "O[C@@H]1C=CC=C(Cl)[C@@H]1O";
+		String expectedSmiles;
+		try
+		{
+			IAtomContainer mol = new SmilesParser(SilentChemObjectBuilder.getInstance())
+					.parseSmiles("C1=CC(=C(C(=C1)Cl)O)O");
+			expectedSmiles = SmilesGenerator.absolute().create(mol);
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+		List<String> s = applySmirks(smirks, smi);
+		Assert.assertTrue(s.contains(expectedSmiles));
+	}
+
 	public void complexCarbonStructure_rule3803_c0630()
 	{
 		/* was not empty in cdk1.4
@@ -27,7 +49,6 @@ public class TestAmbitWithdrawn
 		List<String> s = applySmirks(smirks, smi);
 		Assert.assertTrue("Results should be empty", s.isEmpty());
 	}
-
 
 	public static List<String> applySmirks(String smrk, String smi)
 	{
